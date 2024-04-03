75f6d58fbef7ff8f144aa89a30833d0be5cc42a5
Ben Avrahami <67713588+Ben-Avrahami@users.noreply.github.com>
2024-04-03 13:54:38 +0300

    Client Project Init (#27)

---
+++ Tindira-Client/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <p class="text-3xl font-bold underline text-orange-600">
+      Hello world!!!!!!@
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
