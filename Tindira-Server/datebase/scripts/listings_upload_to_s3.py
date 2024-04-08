07dfceaff72f1723097a4e94914e4f8460d4f461
Gal Ben Artzi <85943633+BenArtzi4@users.noreply.github.com>
2024-04-08 10:35:22 +0300

    create db (#30)

---
+++ Tindira-Server/datebase/scripts/listings_upload_to_s3.py
@@ -0,0 +1,27 @@
+import boto3
+import os
+
+# Initialize S3 client
+s3 = boto3.client('s3')
+
+# Local directory containing listing images
+local_directory = 'C:\\temp\\CloudNetWorkshopUni\\Tindira-Server\\datebase\\Images\\Listings'
+
+# S3 bucket name
+bucket_name = 'tindira'
+
+# Upload images to S3
+for directory_name in os.listdir(local_directory):
+    # Construct the S3 key
+    s3_key_prefix = f'listings/{directory_name}/'
+    
+    # Iterate over image files in the directory
+    for file_name in os.listdir(os.path.join(local_directory, directory_name)):
+        local_file_path = os.path.join(local_directory, directory_name, file_name)
+        s3_key = f'{s3_key_prefix}{file_name}' 
+        # Construct S3 key with the desired structure
+        
+        # Upload the image file to S3
+        s3.upload_file(local_file_path, bucket_name, s3_key)
+
+print("Images uploaded to S3 successfully.")
---
+++ Tindira-Server/datebase/scripts/upload_listings_db.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+def upload_listings_to_dynamodb(file_path, table_name, region_name='us-east-2'):
+    # Create a DynamoDB resource
+    dynamodb = boto3.resource('dynamodb', region_name=region_name)
+
+    # Load JSON data from file
+    with open(file_path, 'r') as file:
+        listings = json.load(file)
+
+    # Get DynamoDB table
+    table = dynamodb.Table(table_name)
+
+    # Upload each listing to DynamoDB
+    for listing in listings:
+        table.put_item(Item=listing)
+
+    print("Listings uploaded to DynamoDB table successfully.")
+
+if __name__ == "__main__":
+    # File path and table name
+    file_path = "C:\\temp\\CloudNetWorkshopUni\\Tindira-Server\\datebase\\listings.json"  # Update the file path
+    table_name = "TindiraListings"
+
+    # Upload listings to DynamoDB table
+    upload_listings_to_dynamodb(file_path, table_name)
