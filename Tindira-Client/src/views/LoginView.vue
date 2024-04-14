bdd625f1b9193924083badc2730a9848be2c691c
Yehonatan Simian <73541314+yonisimian@users.noreply.github.com>
2024-04-14 20:30:57 +0300

    Login Page (#35)
    Co-authored-by: yonisimian <yonisimian@email.com>

---
+++ Tindira-Client/src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <h1 class="text-3xl font-bold text-center text-surface-300 pt-8">Welcome to Tindira</h1>
+    <div class="flex flex-col items-center justify-center h-full">
+      <Button class="my-2 w-full" @click="loginWithGoogle">
+        <Icon icon="fa:google" class="mr-2" />
+        Continue with Google
+      </Button>
+      <Button class="my-2 w-full" @click="loginWithPhoneNumber">
+        <Icon icon="fa:phone" class="mr-2" />
+        Continue with Phone Number
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Button from 'primevue/Button'
+import { Icon } from '@iconify/vue'
+
+const router = useRouter()
+
+const login = () => {
+  router.push('/')
+}
+const loginWithGoogle = () => {
+  login()
+}
+const loginWithPhoneNumber = () => {
+  login()
+}
+</script>
+
+<style scoped></style>
